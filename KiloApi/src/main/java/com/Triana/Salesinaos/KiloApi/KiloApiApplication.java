5b107fe781790d55784eaafe6f9fc912c5695fd7
illoquehambre <illoquehambre@gmail.com>
2022-12-14 10:41:47 +0100

    first commit

---
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/KiloApiApplication.java
@@ -0,0 +1,13 @@
+package com.Triana.Salesinaos.KiloApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiloApiApplication.class, args);
+	}
+
+}
