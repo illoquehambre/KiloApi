725ef80b130d8a654f6c50974e94cdbbe2c134f1
illoquehambre <illoquehambre@gmail.com>
2022-12-15 18:07:36 +0100

    Entidades clase y apotación con su debida relación

--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Aportacion.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Aportacion.java
@@ -1,11 +1,35 @@
 package com.Triana.Salesinaos.KiloApi.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter @Setter
+@Entity
 public class Aportacion {
+
+    @Id@GeneratedValue
+    private Long id;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name= "FK_CLASE_APORTACION"))
+    private Clase clase;
+    public void addClase(Clase c) {
+        this.clase = c;
+        c.getListadoAportaciones().add(this);
+    }
+
+    public void removeClase(Clase c) {
+        c.getListadoAportaciones().remove(this);
+        this.clase = null;
+    }
+
+
 }
--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Clase.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Clase.java
@@ -2,10 +2,27 @@ package com.Triana.Salesinaos.KiloApi.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@Entity
 public class Clase {
+
+    @Id@GeneratedValue
+    private Long id;
+    private String nombre;
+    private String tutor;
+
+    @OneToMany(mappedBy = "clase", cascade = CascadeType.ALL)
+    private List<Aportacion> listadoAportaciones;
+    @PreRemove
+    public void preRemoveClase() {
+        listadoAportaciones.forEach(aport -> aport.setClase(null));
+    }
+
 }
