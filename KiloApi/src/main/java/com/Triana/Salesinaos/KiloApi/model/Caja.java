1041164b029af77964ec6d55a60787c3e1123163
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-12-15 18:17:54 +0100

    entidad caja y destinatario

--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Caja.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Caja.java
@@ -2,10 +2,28 @@ package com.Triana.Salesinaos.KiloApi.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
-public class Caja {
+public class Caja{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String qr, numCaja;
+    private Double kilosTotales;
+
+
+    @ManyToOne
+    @JoinColumn(
+            name = "caja_id",
+            foreignKey = @ForeignKey(name = "FK_CAJA_DESTINATARIO"))
+    private Destinatario destinatario;
+
 }
--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Destinatario.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/Destinatario.java
@@ -2,10 +2,27 @@ package com.Triana.Salesinaos.KiloApi.model;
 
 import lombok.*;
 
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
-public class Destinatario {
+public class Destinatario{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre, direccion, personaContacto, telefono;
+
+    @OneToMany(mappedBy = "destinatario", fetch = FetchType.LAZY)
+    private List<Caja> cajas = new ArrayList<>();
 }
