20c13b28e2ca99a0265188b45f3c00b0248c2d73
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-12-17 00:11:17 +0100

    clases post endpoint terminado. DtoClases Record

---
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/dto/ClaseDto.java
@@ -0,0 +1,23 @@
+package com.Triana.Salesinaos.KiloApi.dto;
+
+import com.Triana.Salesinaos.KiloApi.model.Clase;
+import com.Triana.Salesinaos.KiloApi.model.TipoAlimento;
+import lombok.Builder;
+
+import java.util.Objects;
+
+@Builder
+public record ClaseDto (Long id, String nombre, String tutor){
+    public ClaseDto {
+        Objects.requireNonNull(nombre);
+        Objects.requireNonNull(tutor);
+    }
+
+    public static ClaseDto of(Clase clase){
+        return ClaseDto.builder()
+                .id(clase.getId())
+                .nombre(clase.getNombre())
+                .tutor(clase.getTutor())
+                .build();
+    }
+}
--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/service/ClaseService.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/service/ClaseService.java
@@ -1,6 +1,8 @@
 package com.Triana.Salesinaos.KiloApi.service;
 
+import com.Triana.Salesinaos.KiloApi.dto.ClaseDto;
 import com.Triana.Salesinaos.KiloApi.model.Clase;
+import com.Triana.Salesinaos.KiloApi.model.TipoAlimento;
 import com.Triana.Salesinaos.KiloApi.repository.ClaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,4 +15,16 @@ public class ClaseService {
 
     private final ClaseRepository repository;
     public List<Clase> findAll(){return repository.findAll();}
+
+    public Clase add(Clase clase) {
+        return repository.save(clase);
+    }
+
+    public Clase toClase(ClaseDto claseDto) {
+        return Clase.builder()
+                .id(claseDto.id())
+                .nombre(claseDto.nombre())
+                .tutor(claseDto.tutor())
+                .build();
+    }
 }
