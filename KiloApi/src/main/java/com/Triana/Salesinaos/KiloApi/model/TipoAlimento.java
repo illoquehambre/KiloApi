6394c6ea876be18a4f6840b7ad2958fe8e3fce8e
illoquehambre <illoquehambre@gmail.com>
2022-12-16 09:26:12 +0100

    get all of clase

--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/TipoAlimento.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/model/TipoAlimento.java
@@ -26,4 +26,6 @@ public class TipoAlimento implements Serializable {
 
 
 
+
+
 }
--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/service/ClaseService.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/service/ClaseService.java
@@ -1,9 +1,16 @@
 package com.Triana.Salesinaos.KiloApi.service;
 
+import com.Triana.Salesinaos.KiloApi.model.Clase;
+import com.Triana.Salesinaos.KiloApi.repository.ClaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ClaseService {
+
+    private final ClaseRepository repository;
+    public List<Clase> findAll(){return repository.findAll();}
 }
