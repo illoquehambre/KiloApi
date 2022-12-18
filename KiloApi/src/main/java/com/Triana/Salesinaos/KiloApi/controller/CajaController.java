52e9ff3d1f17c6e1bd13a6908b3f338b7dfcdc45
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-12-18 21:29:36 +0100

    .

--- KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/controller/CajaController.java
+++ KiloApi/src/main/java/com/Triana/Salesinaos/KiloApi/controller/CajaController.java
@@ -1,9 +1,17 @@
 package com.Triana.Salesinaos.KiloApi.controller;
 
 import com.Triana.Salesinaos.KiloApi.dto.CajaDtoConverter;
+import com.Triana.Salesinaos.KiloApi.dto.ClaseDto;
 import com.Triana.Salesinaos.KiloApi.dto.CreateCajaDto;
 import com.Triana.Salesinaos.KiloApi.model.Caja;
 import com.Triana.Salesinaos.KiloApi.service.CajaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +31,25 @@ public class CajaController {
 
     private final CajaDtoConverter cajaDtoConverter;
 
-
+    @Operation(summary = "Este método crea una caja")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se ha creado una nueva caja",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Caja.class)),
+                            examples = @ExampleObject(value = """
+                                            {
+                                                "id": 1,
+                                                "qr": "122234",
+                                                "numCaja": "18",
+                                                "kilosTotales": 0.0,
+                                                "destinatario": null
+                                            }
+                                    """)) }),
+            @ApiResponse(responseCode = "400",
+                    description = "No se han introducido correctamente los datos de caja",
+                    content = @Content),
+    })
     @PostMapping("/")
     public ResponseEntity<Caja> addCaja(@RequestBody CreateCajaDto c){
         if (c.getNumCaja() != ""){
